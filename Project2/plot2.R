460f7e74d1e8c780e95d87b7cb4cbf47dea47acf
J.R. Jasperson <JRTheStudent@gmail.com>
2015-03-19 07:07:05 -0600

    Initial commit

--- Project2/plot2.R
+++ Project2/plot2.R
@@ -1,11 +1,11 @@
-## This script is intended to produce the first of six plots as described in
+## This script is intended to produce the second of six plots as described in
 ## https://class.coursera.org/exdata-012/human_grading/view/courses/973506
 ## /assessments/4/submissions
 
 ## Question 1:
 ## Have total emissions from PM2.5 decreased in the United States from 1999 to 
 ## 2008? Using the base plotting system, make a plot showing the total PM2.5 
-## emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+# emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
 library(plyr)
 library(dplyr)
