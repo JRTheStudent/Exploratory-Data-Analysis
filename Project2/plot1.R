783c0377d00366357dd4271e385b123e505b4901
J.R. Jasperson <JRTheStudent@gmail.com>
2015-03-19 07:06:27 -0600

    Initial commit

--- Project2/plot1.R
+++ Project2/plot1.R
@@ -1,10 +1,16 @@
-## Requirements from: https://class.coursera.org/exdata-012
-## /human_grading/view/courses/973506/assessments/4/submissions
+## This script is intended to produce the first of six plots as described in
+## https://class.coursera.org/exdata-012/human_grading/view/courses/973506
+## /assessments/4/submissions
+
+## Question 1:
+## Have total emissions from PM2.5 decreased in the United States from 1999 to 
+## 2008? Using the base plotting system, make a plot showing the total PM2.5 
+# emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
 library(plyr)
 library(dplyr)
 
-## Setup
+## Setup - only required once; comment out to expedite development.
 
 ## if(!(file.exists("./data") & file.info("./data")$isdir)) dir.create("./data")
 ## sFile <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
@@ -13,14 +19,6 @@ library(dplyr)
 ## write(date(), file = "./data/date_downloaded.txt")
 ## unzip(dFile, exdir = "./data")
 ## file.remove(dFile)
-
-## Have total emissions from PM2.5 decreased in the United States from 1999 to 
-## 2008? Using the base plotting system, make a plot showing the total PM2.5 
-# emission from all sources for each of the years 1999, 2002, 2005, and 2008.
-
-# NEI <- readRDS("./data/summarySCC_PM25.rds")
-# SCC <- readRDS("./data/Source_Classification_Code.rds")
-# data <- join(NEI, SCC, by = "SCC")
 
 data <- join(
     readRDS("./data/summarySCC_PM25.rds")
